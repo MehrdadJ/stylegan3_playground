89c5c3fb9dea64deb040650bd21771276adecb65
MehrdadJ <mehrdad.jahanbanifard@gmail.com>
2024-02-01 12:02:30 +0100

    README edited

--- generate_images.sh
+++ generate_images.sh
@@ -1,16 +1,3 @@
-#!/bin/sh
-
-# Array of class names
-classes=("0" "1" "2" "3" "4")  # Add more class names as needed
-
-# Loop over each class
-for class in "${classes[@]}"; do
-    # Loop over a range of seed values
-    for seed_start in $(seq 1 1 5000); do  # Adjust the range and step as needed
-        seed_end=$((seed_start + 99))  # Adjust this to control the range of seeds per command
-        outdir="gen_l24_256/train/$class"
-        cmd="python gen_images.py --outdir=$outdir --trunc=1 --seeds=$seed_start-$seed_end --class=$class --network=l24-256-snapshot-000660.pkl"
-        echo "Running: $cmd"
-        eval "$cmd"
-    done
-done
+version https://git-lfs.github.com/spec/v1
+oid sha256:23b4aafb337a5abd1443c432879e701c94c4d4604d36c2a5f8fedb5ef6624c8f
+size 620
--- modified_viz/__init__.py
+++ modified_viz/__init__.py
@@ -1,9 +1,3 @@
-# Copyright (c) 2021, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
-#
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto.  Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
-
-# empty
+version https://git-lfs.github.com/spec/v1
+oid sha256:4b46318f70705e2a76a147d008b0a4d793c10b774edb7b5d2241f56ed198370a
+size 448
